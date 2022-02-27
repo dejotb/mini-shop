2a0aae9a07f8587ae4655d7d912134480bb06b7b
dejotb <dejotb@gmail.com>
2022-02-27 13:57:51 +0100

    added class for ProductList

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,6 @@
-// productList.render();
+/* eslint-disable */
+
+'use-strict';
 
 class Product {
   constructor(title, imageUrl, price, description) {
@@ -7,36 +9,10 @@ class Product {
     this.price = price;
     this.description = description;
   }
-
-  render() {
-    const renderHook = document.querySelector('#app');
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
-
-    prodList.insertAdjacentHTML(
-      'beforeEnd',
-      `<li class='product-item'>
-            <div>
-            <img src=${this.imageUrl} alt="${this.title}">
-            </div>
-            <div class="product-item__content">
-            <h2>${this.title}</h2>
-            <h3>$${this.price}</h3>
-            <p>${this.description}</p>
-            <button>Add to Cart</button>
-            </div>
-
-        </li>`
-    );
-
-    renderHook.append(prodList);
-  }
 }
 
-// pillow.render();
-
-const productList = {
-  products: [
+class ProductList {
+  products = [
     new Product(
       'A Pillow',
       'https://media.istockphoto.com/vectors/pillow-vector-id1286304688?k=20&m=1286304688&s=612x612&w=0&h=wOwwOBPyERGVbAn8412_0WErJjXUe_3Iq-NaQjlCliw=',
@@ -46,13 +22,39 @@ const productList = {
 
     new Product(
       'A Carpet',
-      'https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fkrux.tech%2Fwp-content%2Fuploads%2F2021%2F12%2Fkrx0107-krux-space-carpet-xxl-01.jpg&sp=1645653186T752c73b03de9866afd5d7a1d535ec55548c1e0b3acf0dad29083002cb5889bd6',
+      'https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcRguboFDJKD0taDEfmb91oLJ2ZRxw2Pw1sRrn9wyGFqjn2KNcsQ%26s&sp=1645966309Td513f027e6937ac89002090fcf9dec2b214c5b3c03d35cb27c9e4937f23a0da2',
       89.99,
       'A nice carpet'
     ),
-  ],
-};
+  ];
 
-productList.products.forEach((product) => product.render());
+  render() {
+    const renderHook = document.querySelector('#app');
+    const prodList = document.createElement('ul');
+    prodList.className = 'product-list';
 
-// class
+    this.products.forEach(prod => {
+      prodList.insertAdjacentHTML(
+        'beforeEnd',
+        `<li class='product-item'>
+              <div>
+                <img src=${prod.imageUrl} alt="${prod.title}">
+              </div>
+              <div class="product-item__content">
+                <h2>${prod.title}</h2>
+                <h3>$${prod.price}</h3>
+                <p>${prod.description}</p>
+                <button>Add to Cart</button>
+              </div>
+          </li>
+          `
+      );
+    })
+
+    renderHook.append(prodList);
+  }
+}
+
+const productList = new ProductList();
+
+productList.render();
